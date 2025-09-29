38b2617692ac6dbd1e6f6c3c57c319f9fb0ee47c
golalaland <golalaland@proton.me>
2025-09-29 11:28:28 +0100

    app.js

--- app.js
+++ app.js
@@ -330,7 +330,7 @@ function startStarEarning(uid) {
       animateStarsToCounter(5); // spawn 5 flying star particles
     }
 
-  }, 10000); // every 60s
+  }, 60000); // every 60s
 
   // Stop increment when user closes page
   window.addEventListener("beforeunload", () => clearInterval(starInterval));
